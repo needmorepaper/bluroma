48ebeae37616be1988d1068c54676655def36990
penny <penny@hexmani.ac>
2025-10-20 16:44:32 -0400

    Fix static files not loading in production build

--- src/App.tsx
+++ src/App.tsx
@@ -1,11 +1,12 @@
 import "./App.scss";
 
 import { Component } from "solid-js";
+import logo from "/favicon.png?url";
 
 const App: Component = () => {
   return (
     <>
-      <img src="static/favicon.png" width="100" />
+      <img src={logo} width="100" />
       <h1>Bluroma</h1>
     </>
   );
