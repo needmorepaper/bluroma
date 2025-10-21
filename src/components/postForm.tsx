18468c0b01eadd5485c87df05d3e6df344138ec5
penny <penny@hexmani.ac>
2025-10-21 12:15:35 -0400

    Create basic write-only text posts through post form

--- src/components/postForm.tsx
+++ src/components/postForm.tsx
@@ -1,6 +1,53 @@
-import { Component } from "solid-js";
+import { Component, createSignal } from "solid-js";
+import { agent } from "./login";
+import { Client } from "@atcute/client";
+import * as TID from "@atcute/tid";
+import RichtextBuilder from "@atcute/bluesky-richtext-builder";
 
 const PostForm: Component = () => {
+  const [notice, setNotice] = createSignal("");
+  const [text, setText] = createSignal("");
+
+  async function handleSubmit() {
+    const rpc = new Client({ handler: agent });
+    const rawText = text();
+
+    document.querySelector(".submitInfo")?.removeAttribute("hidden");
+
+    try {
+      const res = await rpc.post("com.atproto.repo.createRecord", {
+        input: {
+          collection: "app.bsky.feed.post",
+          record: {
+            $type: "app.bsky.feed.post",
+            text: rawText,
+            langs: ["en"],
+            createdAt: new Date().toISOString(),
+          },
+          repo: agent.sub,
+          rkey: TID.now(),
+        },
+      });
+
+      if (!res.ok) {
+        throw new Error(`${res.data.error}/${res.data.message}`);
+      }
+
+      console.log(res);
+      setNotice("Post successful");
+      setTimeout(() => {
+        setNotice("");
+        document.querySelector(".submitInfo")?.setAttribute("hidden", "");
+      }, 3000);
+    } catch (e: unknown) {
+      if (e instanceof Error) {
+        setNotice(`Failed to post: ${e.message}`);
+      } else {
+        setNotice(`Failed to post`);
+      }
+    }
+  }
+
   return (
     <>
       <form
@@ -14,9 +61,15 @@ const PostForm: Component = () => {
           rows="1"
           cols="1"
           placeholder="The car's on fire, and there's no driver at the wheel..."
+          onchange={(e) => setText(e.target.value)}
         ></textarea>
-        <button type="submit">Post</button>
+        <button type="submit" onclick={handleSubmit}>
+          Post
+        </button>
       </form>
+      <p class="submitInfo" hidden>
+        {notice()}
+      </p>
     </>
   );
 };
